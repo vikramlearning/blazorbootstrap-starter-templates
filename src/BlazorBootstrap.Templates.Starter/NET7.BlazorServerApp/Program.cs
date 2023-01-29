a7b086bd4fda62e6ff193eabf4510a0622f901a4
Vikram Reddy <gvreddy04@hotmail.com>
2023-01-29 19:03:07 +0530

    Getting started - Blazor Server - Setup

--- src/BlazorBootstrap.Templates.Starter/NET7.BlazorServerApp/Program.cs
+++ src/BlazorBootstrap.Templates.Starter/NET7.BlazorServerApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using NET7.BlazorServerApp.Data;
+using BlazorBootstrap; // Add this line
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBlazorBootstrap(); // Add this line
 
 var app = builder.Build();
 
