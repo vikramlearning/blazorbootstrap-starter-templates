33b96f24b81d260a867ecb2c89497e48db3a03c0
Vikram Reddy <gvreddy04@gmail.com>
2023-12-06 21:46:52 +0530

    .NET 8 - Web App - Server - Global

---
+++ src/BlazorBootstrap.Templates.Starter/NET8.BlazorWebAppRenderModeServer/Program.cs
@@ -0,0 +1,29 @@
+using NET8.BlazorWebAppRenderModeServer.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBlazorBootstrap(); // Add this line
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+       .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
