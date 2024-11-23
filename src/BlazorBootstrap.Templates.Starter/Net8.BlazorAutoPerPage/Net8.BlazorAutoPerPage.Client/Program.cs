f1f3ee433f3860a84f72d45a10308b5a6a03f1bb
Vikram Reddy <gvreddy04@gmail.com>
2024-11-23 21:43:34 +0530

    Net8.BlazorAutoPerPage project removed.

--- src/BlazorBootstrap.Templates.Starter/Net8.BlazorAutoPerPage/Net8.BlazorAutoPerPage.Client/Program.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddBlazorBootstrap(); // Add this line
-
-await builder.Build().RunAsync();
