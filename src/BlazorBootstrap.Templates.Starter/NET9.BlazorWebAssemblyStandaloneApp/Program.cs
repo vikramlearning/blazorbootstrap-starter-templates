8a9210e38261383f372dc8807b1f13166fc323ae
Vikram Reddy <gvreddy04@gmail.com>
2024-11-24 11:20:42 +0530

    .NET 9 - Blazor WebAssembly Standalone App - Template

---
+++ src/BlazorBootstrap.Templates.Starter/NET9.BlazorWebAssemblyStandaloneApp/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NET9.BlazorWebAssemblyStandaloneApp;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorBootstrap();
+
+await builder.Build().RunAsync();
